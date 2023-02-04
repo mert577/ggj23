9581dc325b9723399075e2ab6fb9fbb969129530
mert577 <mert.polat@sabanciuniv.edu>
2023-02-04 15:49:04 +0300

    uh oh forgot to commit

---
+++ GGJ2023/Assets/Scripts/Seed.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Seed : MonoBehaviour
+{
+
+    public GameObject explodePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestorySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Activator"){
+            
+            Activate();
+            Destroy(other.gameObject);
+        }
+    }
+
+    IEnumerator DestorySelf(){  
+        yield return new WaitForSeconds(3f);
+        Tween tween = transform.DOScale(0f, 0.3f);
+        yield return tween.WaitForCompletion();
+
+        Destroy(gameObject);
+    }
+    void Activate(){
+
+        //TODO ACTUAL ACTIVATION CODE
+        GameObject explode = Instantiate(explodePrefab, transform.position, Quaternion.identity);
+       UtilityFunctions.TimeStop(0.25f);
+
+        Destroy(gameObject);
+    }
+}
